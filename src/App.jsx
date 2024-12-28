e74750a37696985848a307f0583c094e39729078
Nguyen Ngoc Ninh <nguyenngocninh.3n@gmail.com>
2024-12-28 20:17:13 +0700

    custom dark_light_v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import { Button } from '@mui/material'
+import { Button, useColorScheme } from '@mui/material'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 
 function App() {
+  function ModeToggle() {
+    const { mode, setMode } = useColorScheme()
+    return (
+      <Button
+        onClick={() => {
+          setMode(mode === 'light' ? 'dark' : 'light')
+        }}
+      >
+        {mode === 'light' ? 'Turn dark' : 'Turn light'}
+      </Button>
+    )
+  }
+
   return (
     <>
+    <ModeToggle />
       <div>Xin chao</div>
       <HomeIcon />
       <HomeIcon color="primary" />
